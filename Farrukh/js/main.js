f8e206c8ce4246364fb70c68a1f5fd2b5dffc03b
FarrukhAL <93910422+FarrukhAL@users.noreply.github.com>
2021-11-15 10:06:13 +0100

    Update main.js

--- Farrukh/js/main.js
+++ Farrukh/js/main.js
@@ -227,7 +227,7 @@
 			$.ajax({      	
 
 		      type: "POST",
-		      url: "inc/sendEmail.php",
+		      url: "Farrukh/inc/sendEmail.php",
 		      data: $(form).serialize(),
 		      beforeSend: function() { 
 
